66bcaad25d7f528c9535c0caf9fa7d3d79882357
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2022-09-17 16:55:57 -0400

    Working on controls
    Added Enemy player controls. Also added Gamepad support! This will need
    further work and implementation, but it works!

--- hello-world.js
+++ hello-world.js
@@ -22,6 +22,7 @@ class Sprite {
         this.velocity = velocity
         this.height = 150
         this.direction = 'down'
+        this.lastKey
     }
 
     draw = () =>{
@@ -79,10 +80,17 @@ const keys = {
     },
     w: {
         pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
     }
 }
-
-let lastKey = 'a'
 
 animate = () => {
 
@@ -92,13 +100,26 @@ animate = () => {
     player.update();
     enemy.update();
 
+    //Player Movement
     player.velocity.x = 0;
-    if (keys.a.pressed && lastKey === 'a'){
+    if (keys.a.pressed && player.lastKey === 'a'){
         player.velocity.x = -1
-    } else if (keys.d.pressed && lastKey === 'd'){
+    } else if (keys.d.pressed && player.lastKey === 'd'){
         player.velocity.x = 1
-    } else if (keys.w.pressed === 'w' && lastKey === 'w'){
+    } else if (keys.w.pressed && player.lastKey === 'w'){
         player.velocity.y = -7
+    }
+
+    //Enemy Movement
+    enemy.velocity.x = 0;
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft'){
+        enemy.velocity.x = -1
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
+        enemy.velocity.x = 1
+    }
+
+    if (keys.ArrowUp.pressed){
+        enemy.velocity.y = -7
     }
 }
 
@@ -108,20 +129,35 @@ animate();
 window.addEventListener('keydown', (e) => {
 
     switch(e.key){
+        //Player Keys
         case 'd':
             keys.d.pressed = true;
-            lastKey = e.key
+            player.lastKey = e.key
             break;
         case 'a':
             keys.a.pressed = true;
-            lastKey = e.key
+            player.lastKey = e.key
             break;
         case 'w':
             keys.w.pressed = true;
         case ' ':
             player.velocity.y = -7
             break;
+
+        //Enemy Keys
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
+            enemy.lastKey = e.key
+            break;
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true;
+            enemy.lastKey = e.key
+            break;
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = true;
+            break
     }
+
 
 })
 
@@ -134,9 +170,125 @@ window.addEventListener('keyup', (e) => {
             keys.a.pressed = false;
             break;
         case 'w':
-        keys.w.pressed = false;
+            keys.w.pressed = false;
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false;
+            break;
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false;
+            break;
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false;
             break;
     }
 })
+
+const controllers = Joycon.controllers;
+
+const buttons = Object.values(Joycon.buttonMap);
+
+buttons.forEach( button => {
+
+  controllers.on.press(button, (value) => {
+
+    console.log(button + '! ' + value);
+
+    if (value === 1){
+        switch(button){
+            case 'dpad-right':
+                keys.d.pressed = true;
+                player.lastKey = 'd'
+                break;
+            case 'dpad-left':
+                keys.a.pressed = true;
+                player.lastKey = 'a'
+                break;
+            case 'a':
+                keys.w.pressed = true;
+            case ' ':
+                player.velocity.y = -7
+                break;
+        }
+    }
+
+    if (value === 0){
+        switch(button){
+            case 'dpad-right':
+                keys.d.pressed = false;
+                break;
+            case 'dpad-left':
+                keys.a.pressed = false;
+                break;
+            case 'a':
+                keys.w.pressed = false;
+            case ' ':
+                player.velocity.y = -7
+                break;
+        }
+    }
+
+  });
+})
+
+// controllers.on.press('dpad-up', (value) => {
+
+//     console.log('pressed up!', value); // value is 0 to 1
+//   });
+
+// controllers.on.press('dpad-left', (value) => {
+
+//   console.log('pressed left!', value); // value is 0 to 1
+// });
+
+// controllers.on.press('dpad-right', (value) => {
+
+//     console.log('pressed right!', value); // value is 0 to 1
+//   });
+
+
+// controllers.on.press('dpad-down', (value) => {
+
+//     console.log('pressed down!', value); // value is 0 to 1
+// });
+
+// controllers.on.press('start', (value) => {
+
+//     console.log('pressed start!', value); // value is 0 to 1
+//   });
+
+//   controllers.on.press('start', (value) => {
+
+//     console.log('pressed start!', value); // value is 0 to 1
+//   });
+
+// controllers.on.press('start', (value) => {
+
+//   console.log('pressed start!', value); // value is 0 to 1
+// });
+
+// controllers.on.press('a', (value) => {
+
+//   console.log('pressed a!', value); // value is 0 to 1
+
+// });
+
+// controllers.on.press('b', (value) => {
+
+//   console.log('pressed b!', value); // value is 0 to 1
+
+// });
+
+// controllers.on.press('x', (value) => {
+
+//     console.log('pressed x!', value); // value is 0 to 1
+
+//   });
+
+// controllers.on.press('y', (value) => {
+
+// console.log('pressed y!', value); // value is 0 to 1
+
+// });
 
 
